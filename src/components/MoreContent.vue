ac04797b32184b6a980c88bb0e0af22aeff9492b
A-conanclub <jenson_shan@yeah.net>
2025-01-19 20:51:34 +0800

    更新 MoreContent.vue，优化文本内容结构并调整样式间距

--- src/components/MoreContent.vue
+++ src/components/MoreContent.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="more-content">年少轻狂，总以为天下事，无可不为；岁月蹉跎，终感到天下人，力有尽头。</div>
+  <div class="more-content">年少轻狂，总以为天下事，无可不为；</div>
+  <div class="more-content">岁月蹉跎，终感到天下人，力有尽头。</div>
 </template>
 
 <style lang="scss" scoped>
@@ -7,7 +8,7 @@
   display: flex;
   align-items: center;
   justify-content: center;
-  margin-top: 20px;
+  margin-top: 5px;
   width: 100%;
   height: 100%;
 }
