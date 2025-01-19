285cb54e7a054ded53a1f2568b82f68a710da3f3
A-conanclub <jenson_shan@yeah.net>
2025-01-19 15:08:57 +0800

    更新 MoreContent.vue，合并文本内容并移除多余的样式类

--- src/components/MoreContent.vue
+++ src/components/MoreContent.vue
@@ -1,6 +1,5 @@
 <template>
-  <div class="more-content">年少轻狂，总以为天下事，无可不为</div>
-  <div class="customize">岁月蹉跎，终感到天下人，力有尽头</div>
+  <div class="more-content">年少轻狂，总以为天下事，无可不为；岁月蹉跎，终感到天下人，力有尽头。</div>
 </template>
 
 <style lang="scss" scoped>
@@ -9,14 +8,6 @@
   align-items: center;
   justify-content: center;
   margin-top: 20px;
-  width: 100%;
-  height: 100%;
-}
-.customize {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-top: 5px;
   width: 100%;
   height: 100%;
 }
