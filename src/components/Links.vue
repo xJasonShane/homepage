94097931fe19e527d22a65a25a8ffd14bc71df61
A-conanclub <jenson_shan@yeah.net>
2025-01-23 15:16:01 +0800

    更新 siteLinks.json，移除未使用的链接，添加站点监测链接；更新 Links.vue，引入新图标

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -42,8 +42,8 @@
 
 <script setup>
 import { Icon } from "@vicons/utils";
-// 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Images, File, Rss, Dochub } from "@vicons/fa"; // 注意使用正确的类别
+// 可前往 https://xicons.org/#/ 自行挑选并在此处引入
+import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Images, File, Rss, Dochub, LaptopCode } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -74,6 +74,7 @@ const siteIcon = {
   File,
   Rss,
   Dochub,
+  LaptopCode,
 };
 
 // 链接跳转
