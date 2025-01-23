9f0f68e036a5814fe6f8e9df26a08f4446885af3
A-conanclub <jenson_shan@yeah.net>
2025-01-23 12:23:09 +0800

    更新 siteLinks.json，修改 RSSHub 和文档库的图标；更新 Links.vue，引入新图标

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Images, File } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Images, File, Rss, Dochub } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -72,6 +72,8 @@ const siteIcon = {
   LaptopCode,
   Images,
   File,
+  Rss,
+  Dochub,
 };
 
 // 链接跳转
