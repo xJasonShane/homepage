8f39fd831cfd02df5b9402259770c37f69d48eab
xJasonShane <jenson_shan@yeah.net>
2025-02-26 16:27:26 +0800

    修改文档站为分享站点

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://xicons.org/#/ 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Images, File, Rss, Dochub, Code, FireAlt } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Images, File, Rss, Code, FireAlt, Share } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -73,9 +73,9 @@ const siteIcon = {
   Images,
   File,
   Rss,
-  Dochub,
   Code,
   FireAlt,
+  Share,
 };
 
 // 链接跳转
