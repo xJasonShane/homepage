570422d934a4f83e9d7d6b0bfa6960993030d449
xJasonShane <jenson_shan@yeah.net>
2025-08-20 11:07:36 +0800

    refactor: 更新站点链接配置和图标导入
    调整siteLinks.json中的链接顺序并添加导航链接
    修复Links.vue中重复导入Fire图标的问题

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://xicons.org/#/ 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Images, File, Rss, Code, FireAlt, Share } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Images, File, Rss, Code, FireAlt, Share, Fire } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -76,6 +76,7 @@ const siteIcon = {
   Code,
   FireAlt,
   Share,
+  Fire,
 };
 
 // 链接跳转
